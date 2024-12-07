b32a925bcf7dd9d97d51d6fec597ea8c3872048a
hungdang2905 <20130271@st.hcmuaf.edu.vn>
2024-12-07 19:04:13 +0700

    lan3

---
+++ source_to_csv/src/run/Main.java
@@ -0,0 +1,23 @@
+package run;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Properties;
+
+import controller.LotteryResults;
+import db.DBConnect;
+import db.DBProperties;
+import models.Config;
+import models.Log;
+import models.LotteryItem;
+
+public class Main {
+
+	public void run(String type) {
+
+	}
+
+
+}
